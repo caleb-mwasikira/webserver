caleb-mwasikira/greenlight

Moving on to web servers :)

go.mod
@@ -1,3 +1,5 @@
-module github.com/caleb-mwasikira/web-server
+module github.com/caleb-mwasikira/greenlight
 
 go 1.21.6
+
+require github.com/julienschmidt/httprouter v1.3.0

caleb-mwasikira/webserver

go.mod
@@ -1,3 +1,5 @@
-module github.com/caleb-mwasikira/web-server
+module github.com/caleb-mwasikira/webservers
 
 go 1.21.6
+
+require github.com/julienschmidt/httprouter v1.3.0